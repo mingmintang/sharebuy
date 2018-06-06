84b25cb86c7e727dd76514471cc7e2354f3ae03b
Mingmin Tang <mingmintang0331@gmail.com>
2018-06-06 09:24:45 +0800

    Add Firebase Cloud functions

---
+++ CloudFunctions/functions/index.js
@@ -0,0 +1,60 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.syncNickname = functions.database.ref('/users/{uid}/data/nickname').onUpdate((change, context) => {
+    const uid = context.params.uid;
+    const nickname = change.after.val();
+    const groupsPromise = admin.database().ref(`/users/${uid}/groups`).once('value');
+    return Promise.all([groupsPromise]).then(results => {
+        results[0].forEach(childSnap => {
+            const groupId = childSnap.ref.key;
+            admin.database().ref(`/groups/${groupId}/founderNickname`).set(nickname);
+        });
+        return;
+    });
+});
+
+exports.requestJoinGroup = functions.database.ref('/groups/{groupId}/notify/requestJoinGroup/{id}')
+.onCreate((snapshot, context) => {
+    const noti = snapshot.val();
+    const joinGroupPromise = admin.database().ref(`/groups/${noti.groupId}/users/${noti.fromUid}`).set(false);
+    return Promise.all([joinGroupPromise]).then(results => {
+        return handleNotification(snapshot);
+    });
+});
+
+function handleNotification(snapshot) {
+    const noti = snapshot.val();
+    const tokensPromise = admin.database().ref(`/users/${noti.toUid}/tokens`).once('value');
+    const removePromise = admin.database().ref(snapshot.ref).remove();
+    return Promise.all([tokensPromise, removePromise]).then(results => {
+        let tokens = [];
+        const tokensSnap = results[0];
+        tokensSnap.forEach(childSnap => {
+            if (childSnap.val()) {
+                tokens.push(childSnap.ref.key);
+            }
+        });
+        const payload = {
+            'data' : {
+                'action' : noti.action.toString(),
+                'groupId' : noti.groupId
+            }
+        }
+        return sendMessageToDevices(tokensSnap.ref, tokens, payload);
+    });
+}
+
+function sendMessageToDevices(tokensRef, tokens, payload) {
+    return admin.messaging().sendToDevice(tokens, payload).then(res => {
+        for (let i = 0; i < res.results.length; i++) {
+            const result = res.results[i];
+            if (result.error !== undefined &&
+                result.error.code === 'messaging/invalid-registration-token') {
+                tokensRef.child(tokens[i]).remove();
+            }
+        }
+        return res;
+    });
+}
